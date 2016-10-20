64a7b5af8e39b231ebec838d32f8b1144ce18962
Gunjan Kumar <sngunjan@yahoo.com>
2016-10-20 16:15:40 -0400

    latest code with new service

--- src/handler/userHandler.js
+++ src/handler/userHandler.js
@@ -11,19 +11,47 @@ module.exports = {
     createOrUpdateUser: function (request, reply) {
         let response = new Response;
         let UserModel = mongoose.model("UserProfileCollection", userSchema);
-        let user = new UserModel(request.payload);
+        //let user = new UserModel(request.payload);
 
-        user.save(function (err) {
-            if (err) {
+
+
+        var query = {
+                user_id: request.payload.user_id
+            },
+            update = request.payload,
+            options = {
+                upsert: true,
+                new: true,
+                setDefaultsOnInsert: true
+            };
+
+        // Find the document
+        UserModel.findOneAndUpdate(query, update, options, function (error, result) {
+            if (error) {
                 response.status = response.failure;
                 response.message = " Unable to saved latest data into DB";
             } else {
                 response.status = response.success;
                 response.message = " Saved latest data into DB";
+                response.data = result;
             }
 
             reply(response);
         });
+
+
+
+        // user.save(function (err) {
+        //     if (err) {
+        //         response.status = response.failure;
+        //         response.message = " Unable to saved latest data into DB";
+        //     } else {
+        //         response.status = response.success;
+        //         response.message = " Saved latest data into DB";
+        //     }
+
+        //     reply(response);
+        // });
 
 
     }
